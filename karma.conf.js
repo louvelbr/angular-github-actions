d7479a2fa95a7c6c5a14ee0001576ce25f8f1167
Louve Le Bronec <lebroneclo@cy-tech.fr>
2022-10-09 19:31:04 +0200

    test

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/an'),
+      dir: require('path').join(__dirname, './coverage/angular-github-actions'),
       subdir: '.',
       reporters: [
         { type: 'html' },
